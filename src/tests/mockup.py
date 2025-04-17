9554a38189673dbf6c0fb04f82077d58c92fa62a
ngquocnhatdev9317 <155170039+ngquocnhatdev9317@users.noreply.github.com>
2025-04-17 08:05:55 +0000

    Feature: Update Setting Environment Flow

--- src/tests/mockup.py
+++ src/tests/mockup.py
@@ -2,7 +2,7 @@ from faker import Faker
 from passlib.hash import pbkdf2_sha256
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from core.configs import POSTGRES_DB, URL
+from core.settings import settings
 from database.connection import Connection, get_postgres_container
 from user.model import User
 
@@ -18,7 +18,7 @@ async def mock_user(count=1, **kwargs):
 
         user_instances.append(User(**{"username": username, "email": email, "password": pbkdf2_sha256.hash(password)}))
 
-    conn = Connection(engine=get_postgres_container(f"postgresql+asyncpg://{URL}/{POSTGRES_DB}"))
+    conn = Connection(engine=get_postgres_container(f"postgresql+asyncpg://{settings.url}/{settings.postgres_db}"))
     async with AsyncSession(conn.engine, expire_on_commit=False) as session:
         session.add_all(user_instances)
         await session.commit()
